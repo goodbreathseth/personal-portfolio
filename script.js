e62fe8d19d13504b16aecaa5accfd874632138c5
goodbreathseth <goodbreathseth@gmail.com>
2020-11-24 18:52:30 -0700

    Added codenames

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-
 window.onload = function () {
-	// Fix the height of sections
-	const setHeights = () => {
-		let sections = document.getElementsByTagName("section");
-		for (let i = 0; i < sections.length; i++) {
-			sections[i].style.height = window.innerHeight + "px";
-		}
-	
-		// let background = document.getElementsByClassName("background")[0];
-		// background.style.height = window.innerHeight + "px"
-	}
+  // Fix the height of sections
+  const setHeights = () => {
+    let sections = document.getElementsByTagName("section");
+    let height;
+    if (window.innerWidth < 600) {
+      height = window.innerHeight * 0.55;
+    } else if (window.innerWidth < 900) {
+      height = window.innerHeight * 0.66;
+    } else {
+      height = window.innerHeight;
+    }
+    height += "px";
+    for (let i = 1; i < sections.length; i++) {
+      sections[i].style.height = height;
+    }
 
-	setHeights();
-	// window.addEventListener("resize", setHeights);
+    // let background = document.getElementsByClassName("background")[0];
+    // background.style.height = window.innerHeight + "px"
+  };
+
+  setHeights();
+
+  // Set click event on 'check out my stuff' div
+  document.getElementById("innerWrapper").addEventListener('click', () => {
+    document
+      .getElementById("section_2")
+      .scrollIntoView({ behavior: "smooth", block: "center" });
+  })
 
   // Position stars on screen
   let firstStar = document.getElementsByClassName("star_container")[0];
@@ -33,10 +47,18 @@ window.onload = function () {
     star.style.width = size + "px";
     star.style.height = size + "px";
 
-		// Set color of half the stars
-		if (i % 2 == 0) {
-			star.style.background = "#FFFEF8";
-		}
+    // Set color of half the stars
+    if (i % 2 == 0) {
+      star.style.background = "#FFFEF8";
+      star.className += " animate__animated animate__infinite ";
+      
+      if (i % 3 == 0) {
+        star.className += " animate__flash animate__slower ";
+      }
+      else {
+        star.className += " animate__tada animate__fast ";
+      }
+    }
 
     // Set lax attribute - alternate between two groups that fade in and out
     if (Math.random() >= 0.5) {
@@ -45,17 +67,16 @@ window.onload = function () {
       if (Math.random() >= 0.5) {
         star.setAttribute("data-lax-anchor", "#section_3");
       }
-      let random = Math.random() * (1.2 - .5) + .5;
+      let random = Math.random() * (1.2 - 0.5) + 0.5;
       star.setAttribute(
         "data-lax-opacity",
         "0 1, (vh*" + random + ") 0, (vh*1.5) 1 | loop=(vh*1.5)"
       );
-    }
-    else {
-      let random = Math.random() * (1.8 - .5) + .5;
-			star.setAttribute(
+    } else {
+      let random = Math.random() * (1.8 - 0.5) + 0.5;
+      star.setAttribute(
         "data-lax-opacity",
-        "0 1, (vh*.8) 0, (vh*1.5) 1 | loop=(vh*1.5) offset=(vh*" + random  + ")"
+        "0 1, (vh*.8) 0, (vh*1.5) 1 | loop=(vh*1.5) offset=(vh*" + random + ")"
       );
     }
 
@@ -63,13 +84,9 @@ window.onload = function () {
     starChildren[i].setAttribute("data-depth", Math.random());
   }
 
-
-
   // Set up parallax
   var scene = document.getElementById("scene");
   var parallaxInstance = new Parallax(scene);
-
-
 
   // Setup lax
   lax.setup();
@@ -85,8 +102,8 @@ window.onload = function () {
       skillh2s[i].classList.add("animate__flipOutX");
       setTimeout(() => {
         skillh2s[i].classList.add("hidden");
-          skillh2s[i + 1].classList.remove("hidden");
-          skillh2s[i + 1].classList.add("animate__flipInX");
+        skillh2s[i + 1].classList.remove("hidden");
+        skillh2s[i + 1].classList.add("animate__flipInX");
       }, 500);
     }
   }
@@ -96,63 +113,71 @@ window.onload = function () {
     deleteSpeed: 0,
     lifeLike: true,
     afterComplete: async (step, instance) => {
-      
       header.destroy();
       let skills = new TypeIt(".skills", {
         afterComplete: async () => {
           skills.destroy();
 
-
-          let skillh2s = document.getElementsByClassName("skills")
+          let skillh2s = document.getElementsByClassName("skills");
           let i = 0;
           interval = setInterval(() => {
             if (i == skillh2s.length - 1) {
               // Stop the recursive call
               clearInterval(interval);
 
-              document.getElementById("signalDown").classList.remove("invisible");
-              let checkout = new TypeIt(".checkoutMyStuff", {afterComplete: async() => { checkout.destroy()}}).type("check out my work!").go();
+              document
+                .getElementById("signalDown")
+                .classList.remove("invisible");
+              let checkout = new TypeIt(".checkoutMyStuff", {
+                afterComplete: async () => {
+                  checkout.destroy();
+                },
+              })
+                .type("check out my work!")
+                .go();
             }
 
             addAnimations(i, skillh2s);
-            i++
-          }, 1500)
-        }
-        }).type("a software developer.").go();
-      
+            i++;
+          }, 1500);
+        },
+      })
+        .type("a software developer.")
+        .go();
     },
   })
     .type("hello.")
     .pause(500)
     .break()
     .type("welcome to my site!")
-      .pause(2000)
-      .break()
-      .break()
-      .type("i'm")
-      // .break()
-      // .type("software developer...")
-      // .pause(1000)
-      // .delete(21)
-      // .type("designer...")
-      // .pause(1000)
-      // .delete(11)
-      // .type("frontend engineer...")
-      // .pause(1000)
-      // .delete(20)
-      // .type("CSS ninja...")
-      // .pause(400)
-      // .delete(12)
-      // .type("musician...")
-      // .pause(400)
-      // .delete(11)
-      // .type("ginger...")
-      // .pause(300)
-      // .delete(9)
-      // .pause(1600)
-      // .delete(2)
-      // .break()
-      // .pause(500)
-      // .type("Seth.")
+    .pause(2000)
+    .break()
+    .break()
+    .type("i'm")
+    // .break()
+    // .type("software developer...")
+    // .pause(1000)
+    // .delete(21)
+    // .type("designer...")
+    // .pause(1000)
+    // .delete(11)
+    // .type("frontend engineer...")
+    // .pause(1000)
+    // .delete(20)
+    // .type("CSS ninja...")
+    // .pause(400)
+    // .delete(12)
+    // .type("musician...")
+    // .pause(400)
+    // .delete(11)
+    // .type("ginger...")
+    // .pause(300)
+    // .delete(9)
+    // .pause(1600)
+    // .delete(2)
+    // .break()
+    // .pause(500)
+    // .type("Seth.")
     .go();
-}
+};
+
